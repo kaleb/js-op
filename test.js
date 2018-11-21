c75c53c6f26e3b040e77c74c5db8190ad8fd303c
Kaleb Hornsby <kaleb.hornsby@ruralsource.com>
2018-11-21 08:28:28 -0500

    add basic tests

---
+++ test.js
@@ -0,0 +1,41 @@
+const assert = require('assert');
+const op = require('./op');
+
+assert.ok(op.lt(1, 2));
+assert.ok(op.le(1, 2));
+assert.ok(op.eq(1, 1));
+assert.ok(op.ne(1, 2));
+assert.ok(op.ge(2, 1));
+assert.ok(op.gt(2, 1));
+assert.ok(op.is(1, 1));
+assert.ok(op.isnt(1, 2));
+assert.ok(op.in('in', op));
+assert.ok(op.not(false));
+assert.ok(op.truth(1));
+assert.ok(op.and(1, true));
+assert.ok(op.or(true, false));
+
+assert.strictEqual(op.add(1, 2), 3);
+assert.strictEqual(op.sub(1, 2), -1);
+assert.strictEqual(op.mul(1, 2), 2);
+assert.strictEqual(op.div(1, 2), 0.5);
+assert.strictEqual(op.floordiv(1, 2), 0);
+assert.deepStrictEqual(op.divmod(1, 2), [0, 1]);
+assert.strictEqual(op.mod(1, 2), 1);
+assert.strictEqual(op.pos(-1), -1);
+assert.strictEqual(op.neg(-1), 1);
+
+assert.strictEqual(op.and_(1, 2), 0);
+assert.strictEqual(op.or_(1, 2), 3);
+assert.strictEqual(op.invert(1), -2);
+assert.strictEqual(op.xor(1, 2), 3);
+assert.strictEqual(op.lshift(1, 2), 4);
+assert.strictEqual(op.rshift(1, 2), 0);
+
+assert.strictEqual(op.concat('a', 'b'), 'ab');
+
+assert.strictEqual(typeof op.get(op, 'get'), 'function');
+assert.deepStrictEqual(op.set({}, 'key', 'value'), {key: 'value'});
+
+var o = {key: 'value'};
+assert.deepStrictEqual(op.del(o, 'key'), {});
