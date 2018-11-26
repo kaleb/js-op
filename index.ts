1c2154350f07e175793f5f91196ca9bbade9fe20
Kaleb Hornsby <kaleb@horns.by>
2018-11-25 20:44:12 -0500

    refine linting

--- index.ts
+++ index.ts
@@ -12,10 +12,10 @@ export {default as has} from './has';
 export {default as isIn} from './is-in';
 
 export function not(obj: unknown) {
-    return !(obj);
+    return !obj;
 }
 export function truth(obj: unknown) {
-    return !!(obj);
+    return !!obj;
 }
 
 export function and(a: unknown, b: unknown) {
@@ -25,7 +25,7 @@ export function or(a: unknown, b: unknown) {
     return a || b;
 }
 
-/**
+/*
  * Arithmetic
  * ==========
  */
@@ -57,7 +57,7 @@ export function pos(obj: NotSymbol) {
 export function neg(obj: number) {
     return -obj;
 }
-/**
+/*
  * Bitwise
  * =======
  */
@@ -91,10 +91,10 @@ function getProperty<T, K extends keyof T>(target: T, key: K) {
 }
 
 function getProperties<T, K extends keyof T>(target: T, keys: K[]) {
-    return keys.reduce((result, key) => {
+    return keys.reduce<Partial<T>>((result: Partial<T>, key) => {
         result[key] = target[key];
         return result;
-    }, {} as Partial<T>);
+    }, {});
 }
 
 function setProperty<T, K extends keyof T>(target: T, key: K, value: T[K]) {
@@ -102,11 +102,12 @@ function setProperty<T, K extends keyof T>(target: T, key: K, value: T[K]) {
     return target;
 }
 
-function setProperties<T, K extends keyof T>(target: T, source: Partial<T>) {
-    return Object.assign(target, source) as T;
+function setProperties<T>(target: T, source: Partial<T>): T {
+    // tslint:disable:prefer-object-spread
+    return Object.assign(target, source);
 }
 
-/**
+/*
  * Indexing
  * ========
  */
@@ -114,28 +115,34 @@ export function set<T, K extends keyof T>(target: T, source: Partial<T>): T;
 export function set<T, K extends keyof T>(target: T, key: K, value: T[K]): T;
 export function set<T>(target: T, k: (keyof T|Partial<T>), v?: unknown) {
     const len = arguments.length;
-    if (len < 2 || len > 3 ) {
-        return; // perhaps throw error?
+    // tslint:disable:no-magic-numbers
+    if (len < 2 || len > 3) {
+        return; // Perhaps throw error?
     }
+    // tslint:disable-next-line:switch-default
     switch (len) {
     case 2:
         if (typeof k === 'object' && k !== null) {
             setProperties(target, k);
-            // break so fall-through sets obj[k] = undef
+            // Break so fall-through sets obj[k] = undef
             break;
         }
+        // Falls through
     case 3:
         if (typeof k === 'string' || typeof k === 'number') {
+            // tslint:disable-next-line:no-any
             setProperty(target, k, v as any);
         }
-        break;
     }
     return target;
 }
+
 export function del<T>(target: T, k: keyof T) {
+    // tslint:disable:no-dynamic-delete
     delete target[k];
     return target;
 }
+
 export function get<T>(obj: T, k: keyof T|Partial<T>|Array<keyof T>) {
     if (typeof k === 'object') {
         if (Array.isArray(k)) {
@@ -151,30 +158,23 @@ export function get<T>(obj: T, k: keyof T|Partial<T>|Array<keyof T>) {
  * it will be invoked and its value will be both assigned to the dictionary
  * at the specified key and returned to the caller.
  *
- * @param obj
- * Dictionary object or array
- *
- * @param k
- * Index value
- *
- * @param defval
- * (Optional) Function which returns a default value if that of the
- * dictionary mapped to by the key is not yet defined.
- *
- * @return
- * The value of the dictionary mapped to by the key or the default value
+ * @param obj Dictionary object or array
+ * @param k Index value
+ * @param defval (Optional) Function which returns a default value if that of the dictionary mapped to by the key is
+ * not yet defined.
+ * @return The value of the dictionary mapped to by the key or the default value
  * returned by the function if the dictionary does not contain the key.
  */
 export function getdef<T, K extends keyof T>(obj: T, k: K, defval: () => T[K]) {
     let val = obj[ k ];
 
-    if (( val === undefined ) && defval ) {
+    if ((val === undefined) && defval) {
         val = obj[ k ] = defval();
     }
     return val;
 }
 
-/**
+/*
  * Other
  * =====
  */
