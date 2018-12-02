184ba10a423bfa02145bba8401af84cfe0c47d49
Kaleb Hornsby <kaleb@horns.by>
2018-12-02 07:32:57 -0500

    Update for ts 3.2

--- iterable/map-all.ts
+++ iterable/map-all.ts
@@ -5,6 +5,6 @@ import zip from './zip-all';
  */
 export default function *mapAll<X extends unknown[], Y>(f: (...x: X) => Y, ...iterables: {[K in keyof X]: Iterable<X[K]>}) {
     for (const x of zip(...iterables)) {
-        yield f.apply(undefined, x) as Y;
+        yield f.apply(undefined, x as X);
     }
 }
